d4410de8cc876da063a1d045b40c249683ce0ee4
Remi Laforgue <relaforg@z3r6p3.42lyon.fr>
2026-01-16 10:36:38 +0100

    Change to comply new output

--- ex3/ft_vault_security.py
+++ ex3/ft_vault_security.py
@@ -1,22 +1,23 @@
 if (__name__ == "__main__"):
-    file_name: str = "arcvhive.txt"
+    file_name: str = "archive.txt"
     print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===\n")
     print("Initiating secure vault access...")
     print("Vault connection established with failsafe protocols\n")
     try:
         with open(file_name, "r") as file:
             print("SECURE EXTRACTION:")
-            print("{[}CLASSIFIED{]} Quantum encryption keys recovered")
-            print("{[}CLASSIFIED{]} Archive integrity: 100%")
+            print("[CLASSIFIED] Quantum encryption keys recovered")
+            print("[CLASSIFIED] Archive integrity: 100%")
             conf_info = file.read()
     except Exception:
         print("An error occured")
     print()
     try:
-        with open(file_name, "w") as file:
+        with open(file_name, "a") as file:
             print("SECURE PRESERVATION:")
-            file.write("{[}CLASSIFIED{]} New security protocols archived\n")
-            print("{[}CLASSIFIED{]} New security protocols archived")
+            file.write("[CLASSIFIED] New security protocols archived\n")
+            print("[CLASSIFIED] New security protocols archived")
+        print("Vault automatically sealed upon completion")
     except Exception:
         print("An error occured")
     print("\nAll vault operations completed with maximum security.")
