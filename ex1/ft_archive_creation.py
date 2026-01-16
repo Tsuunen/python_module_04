d5ac63ba2e5c36c090325eceb56463d7549f24e5
Remi Laforgue <relaforg@z3r6p3.42lyon.fr>
2026-01-16 10:27:52 +0100

    Change to comply new output

--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -8,14 +8,12 @@ if (__name__ == "__main__"):
             with open(file_name, "w") as file:
                 print("Initializing new storage unit: new_discovery.txt")
                 print("Storage unit created successfully...\n")
-                print("{[}ENTRY 001{]} New quantum algorithm discovered")
-                file.write(
-                    "{[}ENTRY 001{]} New quantum algorithm discovered\n")
-                print("{[}ENTRY 002{]} Efficiency increased by 347%")
-                file.write("{[}ENTRY 002{]} Efficiency increased by 347%\n")
-                print("{[}ENTRY 003{]} Archived by Data Archivist trainee")
-                file.write(
-                    "{[}ENTRY 003{]} Archived by Data Archivist trainee\n")
+                print("[ENTRY 001] New quantum algorithm discovered")
+                file.write("New quantum algorithm discovered\n")
+                print("[ENTRY 002] Efficiency increased by 347%")
+                file.write("Efficiency increased by 347%\n")
+                print("[ENTRY 003] Archived by Data Archivist trainee")
+                file.write("Archived by Data Archivist trainee\n")
                 print("\nData inscription complete. Storage unit sealed.")
                 print(
                     f"Archive '{file_name}' ready for long-term preservation.")
