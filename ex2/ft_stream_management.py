2e1d075b6c68aff35f900ca43a9aa9655e130e19
Remi Laforgue <relaforg@z3r6p3.42lyon.fr>
2026-01-16 10:33:03 +0100

    Change to comply new output

--- ex2/ft_stream_management.py
+++ ex2/ft_stream_management.py
@@ -6,9 +6,9 @@ if (__name__ == "__main__"):
     id: str = sys.stdin.readline().strip()
     print("Input Stream active. Enter status report: ", end="", flush=True)
     status: str = sys.stdin.readline().strip()
-    print("\n{[}STANDARD{]} Archive status from",
+    print("\n[STANDARD] Archive status from",
           f"{id}: {status}", file=sys.stdout)
-    print("{[}ALERT{]} System diagnostic: Communication channels verified",
+    print("[ALERT] System diagnostic: Communication channels verified",
           file=sys.stderr)
-    print("{[}STANDARD{]} Data transmission complete", file=sys.stdout)
+    print("[STANDARD] Data transmission complete", file=sys.stdout)
     print("\nThree-channel communication test successful.")
